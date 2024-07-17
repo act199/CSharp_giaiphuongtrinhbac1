5f97c3f3d2a123f9489fd3eb79e3f1fbb94772b0
SPC <SPC@17-LAB402>
2024-07-17 19:54:41 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("chương trình giải phương trình bậc 1");
+        double a, b;
+
+        Console.Write(" Nhập số a: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.Write(" Nhập số b: ");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        string ketqua = GiaiPTbac1_cach1(a,b);
+        Console.WriteLine(ketqua);
+
+        GiaiPTbac1_cach2(a, b);
+    }
+    /// <summary>
+    /// Hàm giải phương trình bậc 1
+    /// </summary>
+    /// <param name="a">Đổi số a kiểu double</param>
+    /// <param name="b">Đổi số b kiểu double</param>
+    /// <returnsChuỗi kêt quả</returns>
+        private static string GiaiPTbac1_cach1(double a, double b)
+        {
+            if (a == 0)
+            {
+                 if (b == 0) return "Phương trình vô số nghiệm";
+                 else return "Phương trình vô nghiệm";
+             }
+        else return "Phương trình có 1 nghiệm là" + (-b / a).ToString();
+    }
+    private static void GiaiPTbac1_cach2(double a, double b)
+    {
+        if (a == 0)
+        {
+            if (b == 0) Console.WriteLine("Phương trình vô số nghiệm");
+            else Console.WriteLine ("Phương trình vô nghiệm");
+        }
+        else Console.WriteLine ("Phương trình có 1 nghiệm là" + (-b / a).ToString());
+    }
+}
